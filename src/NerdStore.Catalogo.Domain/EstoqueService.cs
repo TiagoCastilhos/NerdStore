c57b5bf1adf960da2cd7b71d082aa7c79dca7027
TiagoCastilhos <tiagocastilhos66@gmail.com>
2020-01-27 21:05:42 -0300

    commit nosso de cada dia

--- src/NerdStore.Catalogo.Domain/EstoqueService.cs
+++ src/NerdStore.Catalogo.Domain/EstoqueService.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NerdStore.Catalogo.Domain.Events;
+using NerdStore.Core.Bus;
+using System;
 using System.Threading.Tasks;
 
 namespace NerdStore.Catalogo.Domain
@@ -8,10 +8,12 @@ namespace NerdStore.Catalogo.Domain
     public class EstoqueService : IEstoqueService
     {
         private readonly IProdutoRepository _produtoRepository;
+        private readonly IMediatorHandler _bus;
 
-        public EstoqueService(IProdutoRepository produtoRepository)
+        public EstoqueService(IProdutoRepository produtoRepository, IMediatorHandler bus)
         {
             _produtoRepository = produtoRepository;
+            _bus = bus;
         }
 
         public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
@@ -23,6 +25,12 @@ namespace NerdStore.Catalogo.Domain
             if (!produto.PossuiEstoque(quantidade)) return false;
 
             produto.DebitarEstoque(quantidade);
+
+            // TODO: Parametrizar a quantidade minima
+            if (produto.QuantidadeEstoque < 10)
+            {
+                await _bus.PublicarEvento(new ProdutoAbaixoEstoqueEvent(produto.Id, produto.QuantidadeEstoque));
+            }
 
             _produtoRepository.Atualizar(produto);
 
